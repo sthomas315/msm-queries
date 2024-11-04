51a27304816b7dac2a9fe58e6d856fae47a441b6
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-11-04 18:41:56 +0000

    moved routes, fixed tempaltes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", {:controller => "directors", :action => "index"})
-  get("/directors/:the_id", {:controller => "directors", :action => "show"})
+
   get("/directors/youngest", { :controller => "directors", :action => "youngest" })
   get("/directors/eldest", { :controller => "directors", :action => "eldest" })
+
+  get("/directors/:the_id", {:controller => "directors", :action => "show"})
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:the_id", { :controller => "movies", :action => "show" })
