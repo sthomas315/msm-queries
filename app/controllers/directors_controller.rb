09d935ed9291e3a66e22036362ad432c6b2c821f
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-11-04 18:16:17 +0000

    okk

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -5,21 +5,19 @@ end
 
 def show
   the_id = params.fetch("the_id")
-
   matching_records = Director.where({:id => the_id})
   @the_director = matching_records.at(0)
-
   render({:template => "director_templates/details"})
 end
 
 def youngest
-  youngest_records = Director.where.not({ :dob => nil }).order({ :dob => :asc })
+  youngest_records = Director.where.not({ :dob => nil }).order({ :dob => :desc })
   @the_youngest = youngest_records.at(0)
   render({ :template => "director_templates/youngest.html.erb" })
 end
 
 def eldest
-  oldest_records = Director.where.not({ :dob => nil }).order({ :dob => :desc })
+  oldest_records = Director.where.not({ :dob => nil }).order({ :dob => :asc })
   @the_oldest = oldest_records.at(0)
   render({ :template => "director_templates/eldest.html.erb" })
 end
