ab6d5c6773aa28a69b074db63d0109bf63631d13
sthomas315 <147417972+sthomas315@users.noreply.github.com>
2024-11-04 17:58:26 +0000

    more

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -12,11 +12,14 @@ def show
   render({:template => "director_templates/details"})
 end
 
-def junior
+def youngest
   youngest_records = Director.where.not({ :dob => nil }).order({ :dob => :asc })
   @the_youngest = youngest_records.at(0)
   @page_title = "Youngest Director"
+  render({ :template => "director_templates/youngest_and_eldest" })
+end
 
+def eldest
   oldest_records = Director.where.not({ :dob => nil }).order({ :dob => :desc })
   @the_oldest = oldest_records.at(0)
   @page_title = "Oldest Director"
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", {:controller => "directors", :action => "index"})
   get("/directors/:the_id", {:controller => "directors", :action => "show"})
-  get("/directors/youngest", { :controller => "directors", :action => "junior" })
-  get("/directors/eldest", { :controller => "directors", :action => "junior" })
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  get("/directors/eldest", { :controller => "directors", :action => "eldest" })
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:the_id", { :controller => "movies", :action => "show" })
